a1b94777571aa4b6d55b0d05ed50c8e685d6dd9d
Vunnysher <vunnysher@gmail.com>
2019-04-02 18:46:34 +0100

    Code refactor, changed animations from pure css to js/css

--- page-notifications.js
+++ page-notifications.js
@@ -1,6 +1,6 @@
+/*version 1.0.1*/
 function PageNotifications(args = {}) {
     //constructor
-
     if(args.theme=="dark")
         this.theme = "dark";
     else 
@@ -12,7 +12,7 @@ function PageNotifications(args = {}) {
         this.parentDiv = document.body;
     }
     
-
+    this.pageNotifications = [];
     this.lastNotificationIdNumber = 0;
 
     this.notificationsDurationsTimers = {};
@@ -21,164 +21,196 @@ function PageNotifications(args = {}) {
 
     this.deltaLastUpdate =  Date.now();
 
-    
-
     this.container = document.createElement('div');
     this.container.id = 'page-notifications-container';
     this.parentDiv.appendChild(this.container);
 
-    this.notificationsDurationsLoop = setInterval(function() {
+    this.update = setInterval(function() {
         var now = Date.now();
         var deltaTime = now-this.deltaLastUpdate;
         this.deltaLastUpdate = now;
 
-        for(var notificationId in this.notificationsDurationsTimers) {
-            this.notificationsDurationsTimers[notificationId] -= deltaTime;
-            if(this.notificationsDurationsTimers[notificationId]<=0) {
-                this.close(notificationId);
-                delete this.notificationsDurationsTimers[notificationId];
+        for(let id = 0; id < this.pageNotifications.length; id++) {
+            this.pageNotifications[id].update(deltaTime);
+            if(this.pageNotifications[id].toDelete) {
+                this.pageNotifications.splice(id,1);
             }
         }
         
-    }.bind(this),50);
+    }.bind(this),17);
 
     this.push = function(title,content,type,duration) {
-        //validation
-        if(type!="success"&&type!="warning"&&type!="error")
-            type = "info";
-
         this.lastNotificationIdNumber++;
         var notificationId = "pn"+this.lastNotificationIdNumber;
-
-        let notification = document.createElement('div');
-        notification.className = 
-        "page-notifications-body"+
-        " page-notifications-"+this.theme+
-        " page-notifications-"+type;
-
-        notification.setAttribute('notificationid', notificationId);
-
-        let left = document.createElement('div');
-        left.className = "page-notifications-left";
-
-        if(type=="success") left.innerHTML = "&#10003";
-        else if(type=="info") left.innerHTML = "&#9432;";
-        else if(type=="warning") left.innerHTML = "&#9888;";
-        else if(type=="error") left.innerHTML = "&#215;"
-
-
-        let right = document.createElement('div');
-        right.className = "page-notifications-right";
-
-        let timer = document.createElement('div');
-        timer.className = "page-notifications-timer";
-
-        let closeButton = document.createElement('div');
-        closeButton.className = "page-notifications-close";
-        closeButton.innerHTML = "&#215";
-
-        let _this = this;
-        closeButton.onclick = function() {
-            _this.close(notificationId);
-        }
-
-        let h1 = document.createElement('h1');
-        let h2 = document.createElement('h2');
-        let h3 = document.createElement('h3');
-
-        let date = new Date();
-
-        h1.innerHTML = title;
-        h2.innerHTML = content;
-        let minutes = date.getMinutes();
-        if(minutes <= 10) minutes = "0"+minutes;
-        h3.innerHTML = date.getHours() + ":" + minutes;
-    
-        right.appendChild(h1);
-        right.appendChild(closeButton);
-        right.appendChild(h2);
-        right.appendChild(timer);
-        right.appendChild(h3);
-
-        notification.appendChild(left);
-        notification.appendChild(right);
-
-        this.container.appendChild(notification);
-
-        notification.setAttribute("closing","false")
-        notification.style.height = notification.offsetHeight;
-        notification.style.animationName = "notification-showup"
-
-        if(!isNaN(duration)&&duration!=false) {
-            notification.onmouseover = function() {
-                _this.changeDuration(notificationId,'unknown');
-            }
-            notification.onmouseleave = function() {
-                _this.changeDuration(notificationId,duration);
-            }
-            _this.changeDuration(notificationId,duration);
-        } else {
-            _this.changeDuration(notificationId,'unknown');
-        } 
+        this.pageNotifications.push(new PageNotification(notificationId,title,content,type,duration,this.container,this.theme));
     }   
-    this.changeDuration = function(notificationId,duration) {
-        if(!isNaN(duration)) {
-            this.notificationsDurationsTimers[notificationId] = duration;
-            var notifications = this.container.getElementsByClassName('page-notifications-body');
-            
-            for(let notification of notifications) {
-                if(notification.getAttribute('notificationId')==notificationId) {
-                    
-                    notification.getElementsByClassName('page-notifications-timer')[0].style.animationName = "";
-                    setTimeout(function(){
-                        notification.getElementsByClassName('page-notifications-timer')[0].style.width = "330px";
-                        notification.getElementsByClassName('page-notifications-timer')[0].style.animationDuration = duration/1000.0+"s";
-                        notification.getElementsByClassName('page-notifications-timer')[0].style.animationName = "notification-timer-animation";
-                    },0)
-                    this.notificationsDurationsTimeouts[notificationId] = setTimeout(function(){
-                        notification.getElementsByClassName('page-notifications-timer')[0].style.width = "0px";
-                    },duration)
-                }
-            }
-
-        } else {
-            this.notificationsDurationsTimers[notificationId] = "unknown";
-            var notifications = this.container.getElementsByClassName('page-notifications-body');
-            
-            for(let notification of notifications) {
-                if(notification.getAttribute('notificationId')==notificationId) {
-
-                    notification.getElementsByClassName('page-notifications-timer')[0].style.animationName = "";
-                    clearTimeout(this.notificationsDurationsTimeouts[notificationId]);
-                    setTimeout(function(){
-                        notification.getElementsByClassName('page-notifications-timer')[0].style.width = "330px";
-                        notification.getElementsByClassName('page-notifications-timer')[0].style.opacity = "0"
-
-                    },0)
-                }
-            }
-
+    this.closeAll = function(){
+        for(let id = 0; id < this.pageNotifications.length; id++) {
+            this.pageNotifications[id].close();
         }
     }
-    this.close = function(notificationId) {
-        var notifications = this.container.getElementsByClassName('page-notifications-body');
-        for(let notification of notifications) {
-            if(notification.getAttribute("closing")=="false") {
-                if(notification.getAttribute('notificationId')==notificationId) {
-                    notification.style.animationName = "notification-hide"
-                    notification.setAttribute("closing","true")
+}
+
+function PageNotification(pnId,title,content,type,duration,container,theme) {
+    this.pnId = pnId;
+    this.title = title;
+    this.content = content;
+    this.type = type;
+    this.duration = duration;
+    this.durationLeft = duration;
+    this.theme = theme;
+
+    this.heightScale = 0;  //from 0 to 1, variable used to make animations of showing/hiding
+    this.heightCurrentAnimation = 1 // 0 - nothing, 1 - showup, 2 - hide
+    this.heightAnimationProgress = 0
+
+    this.timerScale = 0;  //from 0 to 1, variable used to make animations of showing/hiding
+    this.timerCurrentAnimation = 1 // 0 - nothing, 1 - timing-out
+    this.timerAnimationProgress = 0
+
+    this.notification = document.createElement('div');
+    this.fullHeight = 0;
+
+    this.toDelete = false; // make true if this should be removed from array
+
+    //constructor
+    let _this = this;
+
+    if(type!="success"&&type!="warning"&&type!="error")
+        type = "info";
+
+    this.notification.className = "page-notifications-body"+" page-notifications-"+this.theme+" page-notifications-"+this.type;
+    this.notification.setAttribute('notificationid', this.pnId);
+
+    let left = document.createElement('div');
+    left.className = "page-notifications-left";
+
+    if(type=="success") left.innerHTML = "&#10003";
+    else if(type=="info") left.innerHTML = "&#9432;";
+    else if(type=="warning") left.innerHTML = "&#9888;";
+    else if(type=="error") left.innerHTML = "&#215;"
+
+    let right = document.createElement('div');
+    right.className = "page-notifications-right";
+
+    let timer = document.createElement('div');
+    timer.className = "page-notifications-timer";
+
+    let closeButton = document.createElement('div');
+    closeButton.className = "page-notifications-close";
+    closeButton.innerHTML = "&#215";
+    
+    closeButton.onclick = function() {
+       _this.close();
+    }
+
+    let h1 = document.createElement('h1');
+    let h2 = document.createElement('h2');
+    let h3 = document.createElement('h3');
+
+    let date = new Date();
+
+    h1.innerHTML = title;
+    h2.innerHTML = content;
+    let minutes = date.getMinutes();
+    if(minutes <= 10) minutes = "0"+minutes;
+    h3.innerHTML = date.getHours() + ":" + minutes;
+    
+    right.appendChild(h1);
+    right.appendChild(closeButton);
+    right.appendChild(h2);
+    right.appendChild(timer);
+    right.appendChild(h3);
+
+    this.notification.appendChild(left);
+    this.notification.appendChild(right);
+
+    container.appendChild(this.notification);
+
+    this.fullHeight = this.notification.offsetHeight;
+    this.notification.style.height = "0px";
+
+    this.close = function() {
+        if(this.heightCurrentAnimation==0) {
+            var notifications = container.getElementsByClassName('page-notifications-body');
+            for(let notification of notifications) {
+                if(notification.getAttribute('notificationId')==this.pnId) {
+                    this.heightCurrentAnimation = 2;
+                    this.heightAnimationProgress = 0;
                     setTimeout(function(){
-                        if(notification)
+                        _this.toDelete = true;
                         notification.parentNode.removeChild(notification);
                     },300);
                     return;
                 }
+            }        
+        }
+    }
+    this.changeDuration = function(duration) {
+        if(!isNaN(duration)) {
+            this.duration = duration;
+            this.timerAnimationProgress = 0;
+            this.timerCurrentAnimation = 1;
+        } else if(duration == 'unknown') {
+            this.timerAnimationProgress = 0;
+            this.duration = false;
+            this.timerCurrentAnimation = 0;
+            this.timerScale = 1;
+        } else if(duration == false) {
+            this.timerAnimationProgress = 0;
+            this.duration = false;
+            this.timerCurrentAnimation = 0;
+            this.timerScale = 0;
+        }
+    }
+    this.update = function(deltaTime) {
+        //showing and hiding
+        if(this.heightCurrentAnimation==1) {
+            this.heightAnimationProgress += deltaTime
+            if(this.heightAnimationProgress>= 300) this.heightAnimationProgress = 300;
+            
+            this.heightScale = this.easeOutQuint(this.heightAnimationProgress/300.0);
+            if(this.heightAnimationProgress == 300) this.heightCurrentAnimation = 0;
+        }
+        else if(this.heightCurrentAnimation==2) {
+            this.heightAnimationProgress += deltaTime
+            if(this.heightAnimationProgress>= 300) this.heightAnimationProgress = 300;
+            
+            this.heightScale = 1-this.easeOutQuint(this.heightAnimationProgress/300.0);
+            if(this.heightAnimationProgress == 300) this.heightCurrentAnimation = 0;
+        }
+        this.notification.style.marginTop = this.heightScale*10+"px";
+        this.notification.style.height = this.heightScale*this.fullHeight+"px";
+        //timer
+        if(this.timerCurrentAnimation==1) {
+            this.timerAnimationProgress += deltaTime
+            if(this.timerAnimationProgress>= this.duration) this.timerAnimationProgress = this.duration;
+
+            this.timerScale = 1-this.easeOutQuint(this.timerAnimationProgress/this.duration);
+            if(this.timerAnimationProgress == this.duration) {
+                this.close();
+                this.timerCurrentAnimation = 0;
             }
         }
+        this.notification.getElementsByClassName('page-notifications-timer')[0].style.width = this.timerScale*330+"px";
     }
-    this.closeAll = function(){
-        for(let notificationId in this.notificationsDurationsTimers) {
-            this.close(notificationId);
+
+    this.easeOutQuint = function(t) //source: https://gist.github.com/gre/1650294 
+    { return 1+(--t)*t*t*t*t }
+
+    if(!isNaN(duration)&&duration!=false) {
+        let _this = this;
+
+        this.notification.onmouseover = function() {
+            _this.changeDuration('unknown');
         }
-    }
+        this.notification.onmouseleave = function() {
+            _this.changeDuration(duration);
+        }
+         _this.changeDuration(duration);
+
+    } else {
+        _this.changeDuration(false);
+    } 
+
 }
